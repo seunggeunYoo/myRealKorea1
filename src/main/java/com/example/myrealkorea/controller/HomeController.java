80ca05d2597440879656abd04bbd4dc8b3e1daf6
seunggeunYoo <yooyoosg@naver.com>
2023-03-20 14:28:43 +0900

    ..

--- src/main/java/com/example/myrealkorea/controller/HomeController.java
+++ src/main/java/com/example/myrealkorea/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "호";
+        return "홈페이지";
     }
 }
